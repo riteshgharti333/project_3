ae1481c810c43d23090374a711305ccf08008769
hp <riteshgharti121232@gmail.com>
2025-02-15 09:34:34 +0530

    implement best navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,99 +1,84 @@
 import "./Navbar.scss";
-import { Link, NavLink, useLocation } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import logo from "../../assets/images/logo.png";
 import { useEffect, useState } from "react";
-import MobileMenu from "../MobileMenu/MobileMenu";
 import { IoIosArrowDown } from "react-icons/io";
-
+import { HiMenu, HiX } from "react-icons/hi"; // Mobile menu icons
 import { services } from "../../assets/data";
 
 const Navbar = () => {
   const [scroll, setScroll] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
+  const [dropdownOpen, setDropdownOpen] = useState(false);
 
-  const location = useLocation();
-
-  const path = location.pathname;
   useEffect(() => {
     const handleScroll = () => {
-      if (window.scrollY > 0) {
-        setScroll(true);
-      } else {
-        setScroll(false);
-      }
+      setScroll(window.scrollY > 50);
     };
 
     window.addEventListener("scroll", handleScroll);
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
+    return () => window.removeEventListener("scroll", handleScroll);
   }, []);
 
-  const navbarClass = `navbar ${path === "/contact-us" ? "static" : "fixed"} ${
-    scroll ? "scrolled" : ""
-  }`;
-
   return (
-    <div className={navbarClass}>
-      <div className="nav-left">
-        <div className="logo">
-          <Link to={"/"}>
-            <img src={logo} alt="Logo" />
-          </Link>
-        </div>
+    <nav className={`navbar ${scroll ? "scrolled" : ""}`}>
+      {/* Left: Logo */}
+      <div className="navbar-left">
+        <Link to="/" className="logo">
+          <img src={logo} alt="Company Logo" />
+        </Link>
       </div>
 
-      <div className="mobile-menu">
-        <MobileMenu />
-      </div>
 
-      <div className="nav-right">
-        <NavLink to={"/"} className="nav-link" activeClassName="active-link">
-          Home
-        </NavLink>
+      {/* Right: Navigation Links */}
+      <ul className={`navbar-right ${menuOpen ? "open" : ""}`}>
+        <li>
+          <NavLink to="/" className="nav-link" activeClassName="active-link">
+            Home
+          </NavLink>
+        </li>
 
-        <NavLink
-          to={"/portfolio"}
-          className="nav-link"
-          activeClassName="active-link"
+        <li>
+          <NavLink to="/portfolio" className="nav-link" activeClassName="active-link">
+            Portfolio
+          </NavLink>
+        </li>
+
+        {/* Services Dropdown */}
+        <li
+          className="dropdown"
+          onMouseEnter={() => setDropdownOpen(true)}
+          onMouseLeave={() => setDropdownOpen(false)}
         >
-          Portfolio
-        </NavLink>
-
-        <div className="dropdown-menu">
-          <div className="name-link">
+          <span className="nav-link">
             Services <IoIosArrowDown className="dropdown-icon" />
-            <div className="services-dropdown">
+          </span>
+          {dropdownOpen && (
+            <ul className="dropdown-menu">
               {services.map((service) => (
-                <Link
-                  to={service.link}
-                  key={service.service_name}
-                  className="dropdown-link"
-                >
-                  {service.service_name}
-                </Link>
+                <li key={service.service_name}>
+                  <Link to={service.link} className="dropdown-item">
+                    {service.service_name}
+                  </Link>
+                </li>
               ))}
-            </div>
-          </div>
-        </div>
+            </ul>
+          )}
+        </li>
 
-        <NavLink
-          to={"/about-us"}
-          className="nav-link"
-          activeClassName="active-link"
-        >
-          About Us
-        </NavLink>
+        <li>
+          <NavLink to="/about-us" className="nav-link" activeClassName="active-link">
+            About Us
+          </NavLink>
+        </li>
 
-        <NavLink
-          to={"/contact-us"}
-          className={({ isActive }) =>
-            isActive ? "nav-link active-link" : "nav-link"
-          }
-        >
-          Contact Us
-        </NavLink>
-      </div>
-    </div>
+        <li>
+          <NavLink to="/contact-us" className="nav-link" activeClassName="active-link">
+            Contact Us
+          </NavLink>
+        </li>
+      </ul>
+    </nav>
   );
 };
 
